b5af82e8430c130d11e935ac464c533ac3c9edd4
itolkent <itol.kent@gmail.com>
2025-12-01 00:53:17 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import profileCard from '../src/profileCard.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {profileCard.map((profileCard, index) => (
+        <div key={index} >
+          <div id="main-container">
+            <div id="img-container">
+              <img src={profileCard.image} alt={profileCard.name} />
+              <h2>{profileCard.name}</h2>
+              <h1>{profileCard.jobTitle}</h1>
+            </div>
+
+            <p>{profileCard.bio}</p>
+          </div>
+
+        </div>
+      ))}
     </div>
+
   );
 }
 
---
+++ src/profileCard.js
@@ -0,0 +1,24 @@
+import myImg from './img/me.jpg';
+
+const profileCards = [
+    {
+        image: myImg,
+        name: "ITOL",
+        jobTitle: "Project Manager",
+        bio: "An accomplished Project Manager with a proven track record of leading cross‑functional teams and delivering complex projects on time and within budget. Skilled in strategic planning, resource allocation, and stakeholder communication, they excel at balancing technical requirements with business objectives. Known for their problem‑solving mindset and ability to thrive under pressure, they foster collaboration, drive efficiency, and ensure successful outcomes across diverse industries.."
+    },
+    {
+        image: myImg,
+        name: "kent",
+        jobTitle: "UI/UX Designer",
+        bio: "A creative and detail‑oriented UI/UX Designer with a passion for crafting intuitive, user‑centered digital experiences. Skilled in wireframing, prototyping, and visual design, they excel at blending aesthetics with functionality to deliver seamless interfaces across web and mobile platforms. Known for strong collaboration and problem‑solving abilities, they thrive on transforming complex ideas into simple, engaging designs that enhance usability and delight users."
+    },
+    {
+        image: myImg,
+        name: "K S",
+        jobTitle: "Software Engineer",
+        bio: "A versatile Software Engineer with expertise in designing, developing, and optimizing scalable applications across web and mobile platforms. Skilled in modern programming languages, frameworks, and best practices, they excel at building modular, efficient solutions that enhance performance and user experience. Known for strong problem‑solving abilities and a collaborative mindset, they thrive in dynamic environments, delivering high‑quality code under pressure while driving innovation and continuous improvement."
+    }
+];
+
+export default profileCards;
